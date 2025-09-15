d441bddf6fa35271551832dbb59e3ec49cdd93ba
Cerevia Inc. <cerevia.ai@gmail.com>
2025-09-15 11:27:17 +0900

    Add files via upload

---
+++ head.tsx
@@ -0,0 +1,10 @@
+// app/head.tsx
+export default function Head() {
+  return (
+    <>
+      <title>Cerevia Inc.</title>
+      <meta name="description" content="Transforming Cardiovascular and Neurological Care with AI" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
